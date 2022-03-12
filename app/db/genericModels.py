9cffe70f928e2f5455be79b0c7d2ef77573e19e3
Mohammed Musa <musamo15@students.rowan.edu>
2022-03-11 19:48:45 -0500

    Listings API Calls && Initial Authentication

---
+++ app/db/genericModels.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from typing import Any
+from datetime import datetime
+
+class ListingModel(BaseModel):
+    # Field(...) means the field is required
+    # Union lets u specify multiple types, but matches by the order of types within the list
+    host_id: str = Field(...)
+    category: str = Field(...)
+    item_name: str = Field(...)
+    item_price: str = Field(...)
+    start_date: datetime = Field(...)
+    end_date: datetime = Field(...)
+    attributes: dict = Field(...)
+    
+    @staticmethod
+    def get_keys():
+        return list(('host_id','category','item_name','item_price','start_date','end_date','attributes'))
+
+class RentalsModel(BaseModel):
+    host_id: str = Field(...)
+    leasse_id: str = Field(...)
+    start_date: datetime = Field(...)
+    end_date: datetime = Field(...)
+    
+     
--- app/routers/router.py
+++ app/routers/router.py
@@ -1,20 +1,53 @@
-from fastapi import APIRouter,Request
-from app.db.models import UserModel,TestModel
-from app.db.crud import (create_user,test)
-
+from fastapi import APIRouter,Request,Response,Depends,status
+from fastapi.security import HTTPBearer
+from typing import Union,List
+from app.db.requestModels import ListingRequestModel
+from app.db.responseModels import ListingResponseModel
+from app.db.crud import (create_listing,get_listing, modify_listing,delete_listing,get_all_listings)
+from app.core.utils import VerifyToken
 
 
 rout = APIRouter()
+# Scheme for the Authorization header, lets us require an authentication token for a route
+token_auth_scheme = HTTPBearer()
 
-@rout.get("/test")
+
+@rout.get("/")
 async def root():
     return {"SPOT RENTAL Platform" : "Welcome to our page!!"}
 
-@rout.post("/users",response_model=UserModel,response_model_exclude_none=True)
-async def user_create(request:Request,user:UserModel):
-    return create_user(user)
+@rout.get("/private")
+async def private_route(response : Response, token: str = Depends(token_auth_scheme)):
+    result = VerifyToken(token.credentials).verify()
+    
+    if result.get(status):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+       
+    return result
 
-@rout.post("/test")
-async def user_test(user:TestModel):
-    await test(user)
-    return "done"
+
+"""
+    LISTINGS
+"""
+@rout.get("/getListing", response_model= ListingResponseModel)
+async def user_get_listing(listingId: str,category: str):
+    return  await get_listing(listingId,category)
+
+@rout.get("/getAllListings",response_model=List[ListingResponseModel])
+async def user_get_all_listings(category: str):
+    return await get_all_listings(category)
+
+@rout.post("/createListing",response_model=ListingResponseModel)
+async def user_create_listing(listing:ListingRequestModel):
+   return await create_listing(listing)
+
+@rout.put("/modifyListing",response_model=ListingResponseModel)
+async def user_modify_listing(listingId: str, listing:ListingRequestModel):
+    validListing = {key: value for key, value in listing.dict().items() if value != ""}
+    
+    return await modify_listing(listingId,validListing)
+
+@rout.delete("/deleteListing")
+async def user_delete_listing(listingId: str, category: str):
+   return await delete_listing(listingId,category)
+   
