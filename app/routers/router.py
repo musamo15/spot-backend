2b35dcf588463c66e779bd857c0b24eaaa083e95
anthonyforte <developer@anthonyforte.dev>
2022-05-05 15:33:14 -0400

    added support for image upload to /images

--- app/routers/router.py
+++ app/routers/router.py
@@ -1,12 +1,16 @@
 from datetime import date
-from xmlrpc.client import DateTime
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter, Depends, UploadFile
 from fastapi.security import HTTPBearer
 from typing import List
+from decouple import config
+import boto3
+import botocore
+import uuid
+
 from app.db.models.requestModels import ListingRequestModel
 from app.db.models.responseModels import ListingResponseModel
 from app.db.database import (get_all_categories)
-from app.db.listings import (create_listing,get_listing, modify_listing,delete_listing,get_all_listings,get_listing_item_from_query,get_user_rentals)
+from app.db.listings import (create_listing,get_listing, modify_listing,delete_listing,get_all_listings,get_listing_item_from_query)
 from app.db.users import (get_user_data,update_user_data)
 from app.utils.utilities import (VerifyToken)
 from app.db.sorting import *
@@ -23,13 +27,11 @@ token_auth_scheme = HTTPBearer()
 @rout.get("/users/{user_id}")
 async def user_get_user_data(user_id: str, token: str = Depends(token_auth_scheme)):
     VerifyToken(token.credentials).verify()
-    
     return await get_user_data(user_id)
 
 @rout.put("/users/{users_id}")
 async def user_update_user_data(users_id: str, updated_data: dict ,token: str = Depends(token_auth_scheme)):
     VerifyToken(token.credentials).verify()
-    
     return await update_user_data(users_id,updated_data)
 
 """
@@ -67,7 +69,6 @@ async def user_create_listing(listing:ListingRequestModel, token: str = Depends(
 @rout.put("/listings/{listing_id}",response_model=ListingResponseModel)
 async def user_modify_listing(listing_id, category: str, modifiedListing: dict,token: str = Depends(token_auth_scheme)):
     VerifyToken(token.credentials).verify()
-    
     validListing = {key: value for key, value in modifiedListing.items() if value != ""}
     return await modify_listing(listing_id,category,validListing)
 
@@ -80,3 +81,25 @@ async def user_delete_listing(listing_id, category: str, token: str = Depends(to
 @rout.put("/listings/{listing_id}/rent")
 async def user_rent_listing(listing_id, lessee_id, category: str, start_date, end_date):
     return await add_rental(listing_id, lessee_id, category, start_date, end_date)
+"""
+    IMAGES
+"""
+s3_client = boto3.client('s3',
+    aws_access_key_id=config("AWS_ACCESS_KEY"),
+    aws_secret_access_key=config("AWS_SECRET_KEY")
+)
+
+
+@rout.post("/images")
+async def upload_images(files: List[UploadFile], token: str = Depends(token_auth_scheme)):
+    VerifyToken(token.credentials).verify()
+    urls = []
+    try:
+        for file in files:
+            ext = file.content_type.split("/")[-1]
+            key = str(uuid.uuid4()) + "." + ext
+            s3_client.upload_fileobj(file.file, "spotimagestorage", key)
+            urls.append("https://spotimagestorage.s3.us-east-1.amazonaws.com/" + key)
+    except botocore.exceptions.ClientError as e:
+        print(e)
+    return {"urls": urls}
